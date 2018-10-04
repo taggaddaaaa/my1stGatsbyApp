788479d9d81212e414c6b8f1be34740eafb0d3d1
taggaddaaaa <scaizerg@student.42.fr>
2018-10-04 11:09:37 +0200

    insignificant commit to test it inside visual studio

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My 1st Gatsby App',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>I learn ReactJs using Gatsby.</p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage

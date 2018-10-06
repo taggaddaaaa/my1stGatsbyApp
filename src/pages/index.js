9dbdea3a033fd50894b84db06c5665795410c6d7
taggaddaaaa <scaizerg@student.42.fr>
2018-10-06 09:08:45 +0200

    Delete css to make my own style. Create some className to fit to the Figma File composition and code like it is designed. Change the index.js file to it like the website designcode.io but it will change for my own webapp next time. Add .idea files inside gitignore and delete yarn-error.log that is managed by /node-modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to my new Gatsby site.</p>
-    <p>I learn ReactJs using Gatsby.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>How I learned to design and code React apps</h1>
+        <p>Welcome to my new Gatsby site.</p>
+        <p>I learn ReactJs using Gatsby.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+    
   </div>
 )
 
